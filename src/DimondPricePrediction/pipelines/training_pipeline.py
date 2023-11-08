72eb7d157d9680811f5e7cc692c449f011e8048d
Hritik <hritikkadam74@gmail.com>
2023-11-08 13:48:23 +0530

    files updated

--- src/DimondPricePrediction/pipelines/training_pipeline.py
+++ src/DimondPricePrediction/pipelines/training_pipeline.py
@@ -1,16 +1,14 @@
-from src.DimondPricePrediction.components.data_ingestion import DataIngestion
-
-from src.DimondPricePrediction.components.data_transformation import DataTransformation
-
-from src.DimondPricePrediction.components.model_trainer import ModelTrainer
-
-
 
 import os
 import sys
 from src.DimondPricePrediction.logger import logging
 from src.DimondPricePrediction.exception import customexception
 import pandas as pd
+from src.DimondPricePrediction.components.data_ingestion import DataIngestion
+from src.DimondPricePrediction.components.data_transformation import DataTransformation
+from src.DimondPricePrediction.components.model_trainer import ModelTrainer
+
+
 
 obj=DataIngestion()
 
